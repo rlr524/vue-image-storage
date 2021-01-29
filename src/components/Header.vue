9559654ea93d95a08908d869b719ef3d088cf2c7
Rob Ranf <robranf@gmail.com>
2021-01-28 17:45:56 -0800

    adding header nav and logo

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="header" class="ui secondary menu">
+    <div class="header-logo">
+      <a href="/" class="item">
+        <img class="logo" src="../assets/waifu_logo.svg" alt="Site logo" />
+      </a>
+    </div>
+    <a href="/messages" class="item">Messages</a>
+    <a href="/friends" class="item">Friends</a>
+    <div class="right menu rightmen">
+      <div class="item">
+        <div class="ui icon input">
+          <input type="text" name="" id="" placeholder="Search..." />
+          <i class="search link icon"></i>
+        </div>
+      </div>
+      <a href="/logout" class="ui item">Logout</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {},
+};
+</script>
+
+<style scoped>
+.header-logo {
+  margin-left: 2em;
+}
+.logo {
+  height: 8em;
+  margin: 2em 2em;
+  text-decoration: none;
+}
+.rightmen {
+  padding-right: 2em;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	chainWebpack: (config) => {
 		config.plugin("html").tap((args) => {
-			args[0].title = "Image Storage";
+			args[0].title = "My Waifus";
 			return args;
 		});
 	},
